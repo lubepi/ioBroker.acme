5efffd83b6ca2539dc81a4bda0e984ebc0ec6387
lubepi <lukas.piel@gmail.com>
2026-02-24 02:16:17 +0100

    fix: remove obsolete dns01PpropagationDelay config field

--- src/types.d.ts
+++ src/types.d.ts
@@ -73,7 +73,6 @@ export interface AcmeAdapterConfig {
     dns01Otoken: string;
     dns01Ousername: string;
     dns01OverifyPropagation: boolean;
-    dns01PpropagationDelay: number;
     dns01OcustomerNumber: string;
     dns01OapiPassword: string;
     collections: [
